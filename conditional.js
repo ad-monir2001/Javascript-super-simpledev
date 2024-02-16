2618745c8f73cfcde30a4d06506f3f7418069d5f
Moniruzzaman <ad.monir2001@gmail.com>
2024-02-16 17:00:30 +0600

    up

---
+++ conditional.js
@@ -0,0 +1,9 @@
+//conditional logic. It can be less than, less than or equal,  grater than, grater than or equal and equals to .
+
+var time = 5 ;
+if (time >= 5 ) {
+    console.log("I will go for pray.");
+}
+else {
+    console.log("I won't go.");
+}
