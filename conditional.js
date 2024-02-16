19c1a5f032cdfc30d99aa798099f9d3b19365377
Moniruzzaman <ad.monir2001@gmail.com>
2024-02-16 17:25:33 +0600

    initial commit

--- conditional.js
+++ conditional.js
@@ -1,9 +1,40 @@
 //conditional logic. It can be less than, less than or equal,  grater than, grater than or equal and equals to .
 
-var time = 5 ;
-if (time >= 5 ) {
-    console.log("I will go for pray.");
+// var time = 5 ;
+// if (time >= 5 ) {
+//     console.log("I will go for pray.");
+// }
+// else {
+//     console.log("I won't go.");
+// }
+
+
+// Grade system in javascript.
+
+var marks = 65;
+
+if (marks>=80 && marks<=100){
+    console.log("You have got A+.");
 }
-else {
-    console.log("I won't go.");
+else if (marks<80 && marks>=70){
+    console.log("You have got A.");
+}
+else if(marks<70 && marks>=60){
+    console.log("You have got A-.");
+
+}
+else if(marks<60 && marks>=50){
+    console.log("You have got B.");
+
+}
+else if(marks<50 && marks>=40){
+    console.log("You have got C.");
+
+}
+else if(marks<40 && marks>=33){
+    console.log("You have got D.");
+
+}
+else{
+    console.log("You are failed.");
 }
