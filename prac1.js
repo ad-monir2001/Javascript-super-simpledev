cb44d195c8fc808feb96057a23bfb4a751fb9d32
Moniruzzaman <ad.monir2001@gmail.com>
2024-02-16 10:07:07 +0600

    Initial commit

---
+++ prac1.js
@@ -0,0 +1,6 @@
+var Name = "Moniruzzaman Monir"
+console.log(Name);
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.indexOf("an"));
+console.log(Name.split(" "));
