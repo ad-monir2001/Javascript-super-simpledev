f6df365c8bae96d953d050851b00ee43ddfb8d59
Moniruzzaman <ad.monir2001@gmail.com>
2024-02-16 17:26:48 +0600

    up

--- conditional.js
+++ conditional.js
@@ -1,15 +1,15 @@
-//conditional logic. It can be less than, less than or equal,  grater than, grater than or equal and equals to .
+// ? conditional logic. It can be less than, less than or equal,  grater than, grater than or equal and equals to .
 
-// var time = 5 ;
-// if (time >= 5 ) {
-//     console.log("I will go for pray.");
-// }
-// else {
-//     console.log("I won't go.");
-// }
+var time = 5 ;
+if (time >= 5 ) {
+    console.log("I will go for pray.");
+}
+else {
+    console.log("I won't go.");
+}
 
 
-// Grade system in javascript.
+// !  Grade system in javascript.
 
 var marks = 65;
 
