67f07641c498e3a0b28046eb536f2fe02891360a
Moniruzzaman <ad.monir2001@gmail.com>
2024-02-16 10:15:28 +0600

    None

--- prac1.js
+++ prac1.js
@@ -1,6 +1,18 @@
 var Name = "Moniruzzaman Monir"
 console.log(Name);
-console.log(Name.toLowerCase());
+
+// To lower case some thing. We use it.
+
+console.log(Name.toLowerCase()); 
+
+// To uppercase some thing we use it.
+
 console.log(Name.toUpperCase());
+
+// To see the position of a letter or word . 
+
 console.log(Name.indexOf("an"));
+
+// To slice a word or sentence we use .
+
 console.log(Name.split(" "));
