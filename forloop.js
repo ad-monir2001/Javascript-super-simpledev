6d30b4d1fa61acbc129bc008da09aedb91815f56
Moniruzzaman <ad.monir2001@gmail.com>
2024-02-17 16:52:06 +0600

    init

---
+++ forloop.js
@@ -0,0 +1,15 @@
+// ! for loop 
+
+for(var i = 10; i < 19; i++){
+    console.log(i);
+}
+
+
+// for loop in array
+
+var friend = ["Aziz", "Rana", "Milon", "Anas"];
+
+for(var i = 0; i < friend.length; i++){
+
+    console.log(i);
+}
