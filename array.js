02b5abbebf49592b1a7d2a02ed76c09e1aff4d22
Moniruzzaman <ad.monir2001@gmail.com>
2024-02-17 10:38:24 +0600

    up

---
+++ array.js
@@ -0,0 +1,25 @@
+// ! Array in javascript.
+
+var studentNumber = [33, 82, 54, 25, 23];
+
+// to know a number position in array we use.
+
+console.log(studentNumber.indexOf(23)); // ? expected output 4.
+
+// in a position which array is located we use.
+
+console.log(studentNumber[3]);  // ? expected output 25.
+
+
+// to add a number on the last position in an array.
+console.log(studentNumber);
+
+studentNumber.push(12);
+
+console.log(studentNumber);
+
+// to remove a number on the last position in an array.
+
+studentNumber.pop();
+
+console.log(studentNumber);
