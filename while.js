85dc94d7ecde9c8f61fff739fd994c9af08cfebc
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-01 10:48:46 +0600

    some file deleted

--- while.js
+++
@@ -1,9 +0,0 @@
-// ! while loop 
-
-var a = 10;
-
-while (a< 22){
-    console.log(a);
-    a++
-}
-// ? If you forget to increase the variable used in the condition, the loop will never end. This will crash your browser.
