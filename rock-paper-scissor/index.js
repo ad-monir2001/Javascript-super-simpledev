b9c45f7728fecd07e261ed0859c810f6b878a28a
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-09 07:05:31 +0600

    used function argument and parameter to reduce code on rock paper and scissor game

--- rock-paper-scissor/index.js
+++ rock-paper-scissor/index.js
@@ -1,7 +1,7 @@
 let computerMove = '';
 let result = '';
 
-function computerMv() {
+function computerMov() {
   const randomNumber = Math.random();
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerMove = 'rock';
@@ -12,46 +12,34 @@ function computerMv() {
   }
 }
 
-function rock() {
-  computerMv();
-  if (computerMove === 'rock') {
-    result = 'Tie.';
-  } else if (computerMove === 'paper') {
-    result = 'You lose.';
-  } else if (computerMove === 'scissor') {
-    result = 'You win.';
+function playGame(playerMove) {
+  computerMov();
+  if (playerMove === 'rock') {
+    if (computerMove === 'rock') {
+      result = 'Tie.';
+    } else if (computerMove === 'paper') {
+      result = 'You lose.';
+    } else if (computerMove === 'scissor') {
+      result = 'You win.';
+    }
+  } else if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You win.';
+    } else if (computerMove === 'paper') {
+      result = 'Tie.';
+    } else if (computerMove === 'scissor') {
+      result = 'You lose.';
+    }
+  } else if (playerMove === 'scissor') {
+    if (computerMove === 'rock') {
+      result = 'You lose';
+    } else if (computerMove === 'paper') {
+      result = 'You win.';
+    } else if (computerMove === 'scissor') {
+      result = 'Tie.';
+    }
   }
-  alert(`You have chose rock 
-and computer has chose ${computerMove}
-so ${result}`);
-}
-
-function paper() {
-  computerMv();
-
-  if (computerMove === 'rock') {
-    result = 'You win.';
-  } else if (computerMove === 'paper') {
-    result = 'Tie.';
-  } else if (computerMove === 'scissor') {
-    result = 'You lose.';
-  }
-  alert(`You have chose paper
-and computer has chose ${computerMove}
-so ${result}`);
-}
-
-function scissor() {
-  computerMv();
-
-  if (computerMove === 'rock') {
-    result = 'You lose';
-  } else if (computerMove === 'paper') {
-    result = 'You win.';
-  } else if (computerMove === 'scissor') {
-    result = 'Tie.';
-  }
-  alert(`You have chose scissor 
+  alert(`You have chose ${playerMove} 
 and computer has chose ${computerMove}
 so ${result}`);
 }
