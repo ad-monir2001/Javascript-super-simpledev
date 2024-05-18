e97a4c276ae77dcef0356908e7fbae5bfec0dc38
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-18 07:17:25 +0600

    10d solved

--- exercises/Exercise 10/index.js
+++ exercises/Exercise 10/index.js
@@ -2,3 +2,14 @@ const jsButtonEl = document.querySelector('.js-button');
 
 const check = jsButtonEl.classList.contains('js-button');
 console.log(check);
+
+const gamingEl = document.querySelector('.game');
+
+function toggled() {
+  let checkToggle = gamingEl.classList.contains('is-toggled');
+  if (checkToggle == false) {
+    gamingEl.classList.add('is-toggled');
+  } else {
+    gamingEl.classList.remove('is-toggled');
+  }
+}
