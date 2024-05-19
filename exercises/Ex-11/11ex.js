83e9cf3d57e4b5b3bee2581e6d78d996407961ea
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-19 07:45:10 +0600

    11b solved with the help of solution

--- exercises/Ex-11/11ex.js
+++ exercises/Ex-11/11ex.js
@@ -4,3 +4,11 @@ console.log(nums);
 
 nums[nums.length-1] = 99;
 console.log(nums);
+
+function getLastValue(array) {
+    const lastIndex = array.length-1
+    return array[lastIndex]
+}
+
+console.log(getLastValue([10, 15, 84]));
+console.log(getLastValue(['hello', 'good morning', 'Nice Weather']));
