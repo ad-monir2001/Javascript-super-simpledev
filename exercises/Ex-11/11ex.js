13e2c7264916fc49fb9de9e0187d6e88ffe73e8b
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-19 07:54:09 +0600

    11c solved

--- exercises/Ex-11/11ex.js
+++ exercises/Ex-11/11ex.js
@@ -2,13 +2,21 @@ const nums = [10, 20, 30];
 
 console.log(nums);
 
-nums[nums.length-1] = 99;
+nums[nums.length - 1] = 99;
 console.log(nums);
 
 function getLastValue(array) {
-    const lastIndex = array.length-1
-    return array[lastIndex]
+  const lastIndex = array.length - 1;
+  return array[lastIndex];
 }
 
 console.log(getLastValue([10, 15, 84]));
 console.log(getLastValue(['hello', 'good morning', 'Nice Weather']));
+
+function arraySwap(array) {
+  const SwapArray = array.reverse();
+  return SwapArray;
+}
+
+console.log(arraySwap([40, 5, 54]));
+console.log(arraySwap(['good', 'bad', true, 4]));
