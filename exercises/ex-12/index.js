f039e88ddaa4b72265d0fa4f2d9cdb6b226f361e
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-30 09:18:40 +0600

    12b solved by viewing solution.

--- exercises/ex-12/index.js
+++ exercises/ex-12/index.js
@@ -1,6 +1,21 @@
+/** Solve 12a */
+
 let add = function () {
   console.log(2 + 3);
 };
 
 add();
 add();
+
+/** Solve 12b */
+
+function runTwice(fun) {
+  fun();
+  fun();
+}
+
+runTwice(function() {
+  console.log('12b');
+});
+
+runTwice(add);
